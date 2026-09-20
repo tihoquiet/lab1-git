8b15f4e2a90c3f5a0580417444ef366ef6c25573
Mikhail Teplov <teplov.miha@gmail.com>
2026-09-20 17:02:28 +0700

    Первый коммит: меню и функция варианта 30

---
+++ main.cpp
@@ -0,0 +1,55 @@
+// Лабораторная работа № 1. Вариант 30.
+// Выполнил: Теплов М. Д., группа ПИ-52.
+// Свободное падение
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double g = 9.81;
+
+// Скорость падения
+double fallSpeed(double t) {
+    return g * t;
+}
+
+// ===== Главная функция: меню =====
+int main() {
+    int choice;
+    double t, h;
+
+    do {
+        cout << "\n=== Вариант 30: Свободное падение ===\n";
+        cout << "1. Найти скорость падения\n";
+        cout << "2. Найти высоту падения\n";
+        cout << "3. Найти время падения\n";
+        cout << "0. Выход\n";
+        cout << "Выберите пункт: ";
+
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Введите время t: ";
+            cin >> t;
+            cout << "Скорость v = " << fallSpeed(t) << "\n";
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 0:
+            cout << "Работа завершена.\n";
+            break;
+
+        default:
+            cout << "Такого пункта нет.\n";
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
