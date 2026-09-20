7a4677a2dfa8b8c9379767c9f631cf1cd50ae7db
Mikhail Teplov <teplov.miha@gmail.com>
2026-09-20 17:06:47 +0700

    Второй коммит: добавлена вторая функция и доступ к ней

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ double fallSpeed(double t) {
     return g * t;
 }
 
+// Высота падения
+double fallHeight(double t) {
+    return g * t * t / 2;
+}
+
 // ===== Главная функция: меню =====
 int main() {
     int choice;
@@ -36,6 +41,9 @@ int main() {
             break;
 
         case 2:
+            cout << "Введите время t: ";
+            cin >> t;
+            cout << "Высота h = " << fallHeight(t) << "\n";
             break;
 
         case 3:
